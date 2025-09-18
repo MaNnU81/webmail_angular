957b3d87fd613123a10d113db9844ecde99d7acf
MaNnU <mannuspamm@gmail.com>
2025-09-18 17:39:03 +0200

    s

--- src/app/components/mail-card/mail.component.ts
+++ src/app/components/mail-card/mail.component.ts
@@ -3,7 +3,7 @@ import { Mockmail } from '../../model/mockmail';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-card-message',
+  selector: 'app-mail-card',
   imports: [CommonModule],
   templateUrl: './mail-card.component.html',
   styleUrl: './mail-card.component.scss'
